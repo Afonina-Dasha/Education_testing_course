b9d3695e03623e3cdc2005f5b6550ab21fc5dfde
Павел Умеров <umerov@tecforce.ru>
2021-12-01 17:57:11 +0300

    Замечания

--- src/main/java/lesson1/ProgressiveTax.java
+++ src/main/java/lesson1/ProgressiveTax.java
@@ -2,6 +2,7 @@ package lesson1;
 
 public class ProgressiveTax extends Service implements Taxable{
 
+    //это не прогрессивный налог
     @Override
     public float calculate(float base) {
         return price = (float) (base * 0.15);
