6b089d7ed6cb6a1da99a5518b3b7dec0a9739a17
Павел Умеров <umerov@tecforce.ru>
2021-12-01 17:51:07 +0300

    Замечания

--- src/main/java/lesson1/Service.java
+++ src/main/java/lesson1/Service.java
@@ -1,5 +1,6 @@
 package lesson1;
 
+//какой смысл в этом классе? Я бы его убрал и сразу имплентировал интерфейс
 public class Service implements Taxable {
 
     public float price;
--- src/main/java/lesson1/Taxable.java
+++ src/main/java/lesson1/Taxable.java
@@ -2,6 +2,7 @@ package lesson1;
 
 public interface Taxable {
 
+    //как смысл в этой дефолтной реализации? тут метод, должен быть абстрактным
     default float calculate(float base){
         return base;
 
