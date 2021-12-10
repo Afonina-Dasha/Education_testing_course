4511194aed85404dfdb2dd9de4fff846bcaa4926
Dasha Nesterova <nesterova@intellex.ru>
2021-12-10 17:34:14 +0400

    Добавление файликов с ФИО

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -1,11 +1,6 @@
 package lesson1;
 
 public class Main {
-    /*
-    * 1. Считываем значения из файлов и кладем в List = получим 1000 ФИО
-    * 2.
-    *
-    * */
 
     public static void main(String[] args) {
 
---
+++ src/main/java/lesson1/RandomValue.java
@@ -0,0 +1,33 @@
+package lesson1;
+
+import java.io.*;
+
+public class RandomValue {
+    String path;
+    FileReader file;
+
+    public RandomValue(String path) {
+        this.path = path;
+        try {
+            file = new FileReader(путь к файлу);
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+    }
+    public String getRandom(){
+        BufferedReader br = new BufferedReader(file);
+        String value;
+        try {
+            value = br.readLine();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+
+}
