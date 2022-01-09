0d3bfbedb754406a8a7c95475d7280b0de500cbf
Dasha Nesterova <nesterova@intellex.ru>
2022-01-09 18:52:15 +0400

    Реализация заполнения класса случайным числом.

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -1,9 +1,31 @@
 package lesson1;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Set<Person> set = new HashSet<>();
+        List<Person> personList = new ArrayList<>();
+        IntStream.range(0,99).forEach(i -> {
+            personList.add(new Person());
+        });
+        Collections.sort(personList, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                if(o1.age > o2.age){
+                    return 1;
+                }
+                else if(o1.age < o2.age){
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            }
+        });
     }
 }
 
--- src/main/java/lesson1/Person.java
+++ src/main/java/lesson1/Person.java
@@ -1,7 +1,22 @@
 package lesson1;
 
-public class Person {
-    private int age;
-    private String name;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Random;
 
+public class Person implements Comparable<Person>{
+    Integer age;
+    String name;
+    HashMap<String, String> fio;
+
+    public Person() {
+        age = new Random().nextInt(99);
+        //fio = "123";
+
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.age;
+    }
 }
