e32c5dac27fe760799c19f886811e94e2e89ec9e
Dasha Nesterova <nesterova@intellex.ru>
2022-01-14 12:03:41 +0400

    Парсинг первой части

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -1,8 +1,21 @@
 package lesson1;
 
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper;
+        HashMap<String, Object> object = new HashMap<>();
+        object.put("squadName","Super hero squad");
+        object.put("homeTown", "Metro City");
+        object.put("formed", 2016);
+        object.put("secretBase", "Super tower");
+        object.put("active", true);
+        List<Map<String, Object>> members = new ArrayList<>();
+        HashMap<String, Object> member1 = new HashMap<>();
+        member1.put("name", "Molecule Man");
 
 
     }
