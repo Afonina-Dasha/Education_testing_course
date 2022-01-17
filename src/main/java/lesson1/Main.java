f6b83dc36ca6be8a5427a652f60e8b0fee4fce09
Dasha Nesterova <nesterova@intellex.ru>
2022-01-17 11:02:58 +0400

    Реализовано чтение из файла.

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -2,6 +2,7 @@ package lesson1;
 
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,58 +13,9 @@ import java.util.stream.IntStream;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
-        //1
-        RandomValue secondRandomGenerator = new RandomValue("src/main/resources/SecondNames.txt");
-        RandomValue nameRandomGenerator = new RandomValue("src/main/resources/FirstNames.txt");
-        RandomValue otchestvoRandomGenerator = new RandomValue("src/main/resources/MiddleNames.txt");
-        //2
-        List<FIO> fioList = new ArrayList<>();
-
-        IntStream.range(0, 100).forEach(i -> {
-            String second = secondRandomGenerator.getRandom();
-            String name = nameRandomGenerator.getRandom();
-            String otchestvo = otchestvoRandomGenerator.getRandom();
-            FIO fio = new FIO(second, name, otchestvo);
-            fioList.add(fio);
-        });
-
-
-        //3
-        String fileName = String.format("out/tests/fios-%s.txt", LocalDateTime.now().toString().replace(":", "_"));
-        File file = null;
-        try {
-            file = Files.createFile(Paths.get(fileName)).toFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        FileOutputStream fo = null;
-        try {
-            fo = new FileOutputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            StringBuilder stringBuilder = new StringBuilder();
-            for (FIO fio : fioList) {
-                stringBuilder.append(fio.toString() + "\n");
-            }
-            byte[] bytes = stringBuilder.toString().getBytes();
-            fo.write(bytes);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fo.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        //List<String> persons = Arrays.asList(fileName.split(""));
-        List<String> persons = List.of(fileName.split(""));
+        List<String> persons = Files.readAllLines(new File("src/main/resources/fios-2022-01-12T11_57_35.803.txt").toPath());
 
         List<Map<String, String>> parsedPerson = new ArrayList<>();
         persons.forEach(i -> {
